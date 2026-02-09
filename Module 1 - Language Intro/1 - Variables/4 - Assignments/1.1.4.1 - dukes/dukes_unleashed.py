1442ea0782e85fb83ed36439ec225e34d2772e37
JoshRFranco <francojr@dukes.jmu.edu>
2026-02-09 15:13:53 -0500

    Update dukes_unleashed.py

--- Module 1 - Language Intro/1 - Variables/4 - Assignments/1.1.4.1 - dukes/dukes_unleashed.py
+++ Module 1 - Language Intro/1 - Variables/4 - Assignments/1.1.4.1 - dukes/dukes_unleashed.py
@@ -4,6 +4,10 @@ Using the [2022 - 2023 JMU Cost of Attendance](https://www.jmu.edu/financialaid/
 calculate how much a rich alumnus would have to give to pay for one full year (all costs) for an in-state student
 and an out-of-state student. Store your final answer in the variables: "in_state_gift" and "out_state_gift".
 
+JMU 2022-2023 Annual:
+In-state total cost: 30792 USD
+Out-of-state total cost: 47882 USD
+
 Note: this problem does not require the "compounding interest" formula from the previous problem.
 
 """
@@ -17,3 +21,7 @@ out_state_gift = 47882 / 0.05
 print(in_state_gift)
 print(out_state_gift)
 
+<<<<<<< HEAD
+=======
+out_state_gift = 0
+>>>>>>> e63cb12421db9070a37ef3ae800e2180542bd36b
